8af6e14334de33efa53e91799c4883fb33060695
tangp <18780290431@163.com>
2021-10-09 10:09:23 +0800

    stupid

--- platform.mk
+++ platform.mk
@@ -23,7 +23,7 @@ PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-arrow
 
 # Permissions
 PRODUCT_COPY_FILES += \
